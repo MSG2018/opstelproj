bee2c6fbed5f8c0aae2368185ec29abae3c739d1
MGautier <35888388+MSG2018@users.noreply.github.com>
2018-02-01 12:55:19 -0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,19 +1,5 @@
 <?php
-/**
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yiiproj',
-    'username' => 'm***',
-    'password' => '****',
-    'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];
-
-**/
 
 
 return [
